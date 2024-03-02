8be0ef919f5006017b67be8b569b747b2f2615a2
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2024-03-02 19:11:50 -0300

    1

--- twilioController.js
+++ twilioController.js
@@ -1,14 +1,13 @@
 // twilioController.js
 const twilio = require("twilio");
 const { accountSid, authToken } = require("./database");
-const { getGreeting, add9ToBrazilianPhoneNumber } = require("./utils");
+const { add9ToBrazilianPhoneNumber } = require("./utils");
 
 const client = twilio(accountSid, authToken);
 const processedMessages = new Set();
 
 const waitForUserResponse = async (senderPhoneNumber) => {
-  const timeout = 30000; // Tempo limite  (30 segundos)
-
+  const timeout = 30000; // Tempo limite (30 segundos)
   const startTime = new Date().getTime();
 
   try {
@@ -19,76 +18,28 @@ const waitForUserResponse = async (senderPhoneNumber) => {
       });
 
       if (messages.length > 0) {
-        // Existe uma resposta do usuário
-        return messages[0].body;
+        return messages[0].body; // Retorna a resposta do usuário
       }
 
-      // Aguardar antes de verificar novamente
       await new Promise((resolve) => setTimeout(resolve, 1000)); // Aguarda 1 segundo
     }
 
-    // Nenhuma resposta do usuário dentro do tempo limite
-    return null;
+    return null; // Nenhuma resposta do usuário dentro do tempo limite
   } catch (error) {
     console.error(`Erro ao aguardar resposta do usuário: ${error.message}`);
     throw error;
   }
 };
 
-const sendGreetingAndAskForOrder = async (to) => {
-  try {
-    // Lógica para determinar a saudação com base no horário do dia
-    const greeting = getGreeting();
-
-    // Enviar saudação
-    await client.messages.create({
-      body: greeting,
-      from: "whatsapp:+14155238886", // Substitua pelo seu número Twilio
-      to: to,
-    });
-
-    // Esperar um pouco antes de enviar a próxima mensagem
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    // Pergunta sobre o número do pedido
-    const orderQuestion = "Qual é o número do seu pedido?";
-    await client.messages.create({
-      body: orderQuestion,
-      from: "whatsapp:+14155238886", // Substitua pelo seu número Twilio
-      to: to,
-    });
-
-    console.log(
-      "Saudação e pergunta sobre o número do pedido sendo enviadas..."
-    );
-
-    // Esperar pela resposta do usuário
-    const userResponse = await waitForUserResponse(to);
-
-    if (userResponse !== null) {
-      console.log(`Resposta do usuário: ${userResponse}`);
-    } else {
-      console.log("Nenhuma resposta do usuário dentro do tempo limite.");
-    }
-
-    console.log("Saudação e pergunta enviadas com sucesso.");
-  } catch (error) {
-    console.error(
-      `Erro ao enviar saudação e pergunta sobre o número do pedido: ${error.message}`
-    );
-    throw error;
-  }
-};
 const handleIncomingMessage = async (req, res) => {
   try {
     if (!req.body || !req.body.Body) {
       console.error(
-        `Mensagem inválida recebida. Corpo da requisição está vazio. ${req.body}`
+        "Mensagem inválida recebida. Corpo da requisição está vazio."
       );
       return res.sendStatus(400);
     }
 
-    const twilioMessage = req.body.Body.toLowerCase();
     const senderPhoneNumber = req.body.From;
     const messageSid = req.body.MessageSid;
 
@@ -102,8 +53,25 @@ const handleIncomingMessage = async (req, res) => {
     const senderPhoneNumberWith9 =
       add9ToBrazilianPhoneNumber(senderPhoneNumber);
 
-    await sendGreetingAndAskForOrder(senderPhoneNumberWith9);
+    // Pergunta sobre o número do pedido
+    await client.messages.create({
+      body: "Oi! Qual é o número do seu pedido?",
+      from: "whatsapp:+14155238886",
+      to: senderPhoneNumberWith9,
+    });
 
+    console.log("Pergunta sobre o número do pedido enviada...");
+
+    // Esperar pela resposta do usuário
+    const userResponse = await waitForUserResponse(senderPhoneNumberWith9);
+
+    if (userResponse !== null) {
+      console.log(`Resposta do usuário: ${userResponse}`);
+    } else {
+      console.log("Nenhuma resposta do usuário dentro do tempo limite.");
+    }
+
+    console.log("Pergunta sobre o número do pedido enviada com sucesso.");
     return res.sendStatus(200);
   } catch (error) {
     console.error("Erro ao processar mensagem:", error.message || error);
