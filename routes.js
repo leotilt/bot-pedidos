8937c720ccb3ab619d7a121d3b9baed8d10b40d7
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2024-03-10 10:51:58 -0300

    1

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const controller = require("./controller/twilioController");
+const controller = require("./controllers/twilioController");
 
 const router = express.Router();
 
