2be89da40c600e843008f1ba1ee2096c21e7406b
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2024-04-03 20:55:27 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 const user = "leonardodevdias";
 const password = "KmohLBJyQ7ztzN0N";
 
+require("dotenv").config();
+
 module.exports = {
-  accountSid: "AC2f6509d080e22ed45ba3b14c83f0e30c",
-  authToken: "a53d1540ca71c107af4ba6ae61c53b00",
-  twilioPhoneNumber: "+14155238886",
-  uri: `mongodb+srv://${user}:${password}@cluster0.tb6fbpl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+  accountSid: process.env.ACCOUNT_SID,
+  authToken: process.env.AUTH_TOKEN,
+  twilioPhoneNumber: process.env.TWILIO_PHONE_NUMBER,
+  uri: process.env.MONGODB_URI,
 };
